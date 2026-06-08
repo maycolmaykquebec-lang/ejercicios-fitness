19ee573f199d8882e4d06d53e365394f9d6ca3f9
Yamil Mamani Quisbert <maycolmaykquebec@gmail.com>
2026-06-08 09:25:44 -0400

    componente ExerciseTitle realizado

---
+++ src/components/atoms/ExerciseTitle.jsx
@@ -0,0 +1,9 @@
+const ExerciseTitle = ({ text }) => {
+  return (
+    <h3 style={{ color: "#ffffff", margin: 0, fontSize: "18px", fontWeight: "bold" }}>
+      {text}
+    </h3>
+  );
+};
+
+export default ExerciseTitle;
