a2731fc4609833e8b8a99066e56fd97e8119d52e
Yamil Mamani Quisbert <maycolmaykquebec@gmail.com>
2026-06-08 09:29:35 -0400

    componente ExerciseSubtitle realizado

---
+++ src/components/atoms/ExerciseSubtitle.jsx
@@ -0,0 +1,9 @@
+const ExerciseSubtitle = ({ text }) => {
+  return (
+    <p style={{ color: "#aaaaaa", margin: 0, fontSize: "14px" }}>
+      {text}
+    </p>
+  );
+};
+
+export default ExerciseSubtitle;
